cd963b62ab93906a0fe9189dcc833f1fbe7344fb
Akash goyal <124559787+akakashgoyal@users.noreply.github.com>
2026-01-28 16:37:58 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
               <img 
                 src={strikeLogo} 
                 alt="Strike Films" 
-                className="h-14 w-auto object-contain"
+                className="h-20 w-auto object-contain"
               />
             </motion.div>
           </Link>
